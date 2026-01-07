cdfcea4d3a45155b9b0b54ec82382b34ad7df94a
codeandlifewithkp <kp.codeandlife@gmail.com>
2026-01-07 17:24:03 +0530

    modify src

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+    return (
+        <footer>
+            &copy; for 2026.
+        </footer>
+    );
+}
+
+export default Footer;
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,10 +1,12 @@
 import Header from "../Components/Header";
+import Footer from "../Components/Footer";
 
 function Home () {
     return (
         <>
             <Header />
             <h1>Home</h1>
+            <Footer />
         </>
     );
 }
