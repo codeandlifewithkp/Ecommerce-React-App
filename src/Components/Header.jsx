7cfac60f0316245821a057904d32457e486700d9
Kalpesh Mevada <kp.codeandlife@gmail.com>
2026-01-17 17:58:56 +0530

    modify Header & Home

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -4,11 +4,12 @@ import Modal from './Modal';
 import icon from './icon.png';
 import './css/Header.css';
 import { FaCartShopping } from 'react-icons/fa6';
-import { FaSearch } from "react-icons/fa";
+import { FaSearch, FaBars } from "react-icons/fa";
 import { BiSolidUserCircle } from "react-icons/bi";
 
 function Header() {
     const [open, setOpen] = useState(false);
+    const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
     return (
         <header>
             <Link to={'/'} className='brand-container'>
@@ -19,6 +20,8 @@ function Header() {
                     <span className='brand-subtitle'>Gaming Accessories</span>
                 </div>
             </Link>
+
+            {/* Desktop Navigation */}
             <nav className='navbar'>
                 <NavLink to={'/'}>Home</NavLink>
                 <NavLink to={'/store/'}>Store</NavLink>
@@ -33,7 +36,31 @@ function Header() {
                     <BiSolidUserCircle size={30} />
                 </button>
             </nav>
+
+            {/* Mobile Navigation */}
+            <nav className='navbar-mobile'>
+                <button id='btn' onClick={() => setMobileMenuOpen(!mobileMenuOpen)}>
+                    <FaBars />
+                </button>
+                <div className='navbar-mobile-links' style={{ display: mobileMenuOpen ? 'flex' : 'none' }}>
+                    <div className='navbar-mobile-search'>
+                        <input type='text' placeholder='Search' />
+                        <FaSearch />
+                    </div>
+                    <NavLink to={'/'}>Home</NavLink>
+                    <NavLink to={'/store/'}>Store</NavLink>
+                    <NavLink to={'/custom-builds/'}>Custom Builds</NavLink>
+                </div>
+            </nav>
+            <div className='navbar-mobile-actions'>
+                <button id='btn'>
+                    <FaCartShopping />
+                </button>
+                <button id='btn' onClick={() => setOpen(true)}>
+                    <BiSolidUserCircle size={30} />
+                </button>
             <Modal open={open} onClose={() => setOpen(false)} />
+            </div>
         </header>
     );
 }
