0f0eeceb267555c5a20e5dfa224ff8367f0adc54
Kalpesh Mevada <kp.codeandlife@gmail.com>
2026-01-26 13:22:08 +0530

    modify App

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
-import { useState, useEffect } from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './Pages/Home';
-import Store from './Pages/Store';
-import CustomBuilds from './Pages/CustomBuilds';
-import Click from './Components/Click';
-import { Change } from './Components/Change';
-import './App.css';
+import { useState, useEffect } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home";
+import Store from "./Pages/Store";
+import CustomBuilds from "./Pages/CustomBuilds";
+import Click from "./Components/Click";
+import { Change } from "./Components/Change";
+import "./App.css";
 
 function App() {
-  const [theme, setTheme] = useState('dark');
+  const [theme, setTheme] = useState("dark");
 
   const toggleTheme = () => {
-    setTheme((curr) => (curr === 'dark' ? 'light' : 'dark'));
+    setTheme((curr) => (curr === "dark" ? "light" : "dark"));
   };
 
   useEffect(() => {
