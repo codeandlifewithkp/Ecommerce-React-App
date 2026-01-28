362ce1264123d995af322694656ea1f37aae1a47
Kalpesh Mevada <kp.codeandlife@gmail.com>
2026-01-28 19:05:47 +0530

    modify Store

--- src/Pages/Store.jsx
+++ src/Pages/Store.jsx
@@ -1,12 +1,15 @@
 import Header from "../Components/Header";
+import Footer from "../Components/Footer";
+import "./Styles/Store.css";
 
 function Store() {
-    return (
-        <>
-            <Header />
-            <h1>Store</h1>
-        </>
-    );
+  return (
+    <>
+      <Header />
+      <h1>Store</h1>
+      <Footer />
+    </>
+  );
 }
 
 export default Store;
